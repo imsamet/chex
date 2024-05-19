e5452031169d95bc276ecf4f48c00a4e39e9e2e6
Samet Çetin <46278098+imsamet@users.noreply.github.com>
2024-05-19 19:54:57 +0300

    responsive settings of footer were made

--- components/layout/footer/index.tsx
+++ components/layout/footer/index.tsx
@@ -6,18 +6,16 @@ export const Footer: React.FC = ({}) => {
   return (
     <footer className="bg-black">
       <div className="container mx-auto">
-        <div className="flex justify-between items-center py-3">
+        <div className="flex lg:flex-row flex-col justify-between items-center py-3">
           <Link href="/">
             <Logo className="w-24 h-auto" />
           </Link>
-          <div className="flex gap-3">
-            <span className="text-white text-xs pr-3 border-r border-r-white">
+          <div className="flex gap-3 lg:flex-row flex-col items-center">
+            <span className="text-white text-xs pr-3">
               Word Buzz Entertainment Inc. © Since 1923 - 2024
             </span>
-            <span className="text-white text-xs pr-3 border-r border-r-white">
-              Samet Çetin
-            </span>
-            <span className="text-white text-xs pr-3 border-r border-r-white">
+            <span className="text-white text-xs pr-3">Samet Çetin</span>
+            <span className="text-white text-xs pr-3">
               imsametcetin@gmail.com
             </span>
             <span className="text-white text-xs">0507 834 0555</span>
